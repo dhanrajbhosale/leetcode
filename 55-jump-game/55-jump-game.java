08cbb58e14f38424522a1d45cf03fd550baaed10
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-04-08 00:14:27 +0530

    Time: 2 ms (87.00%), Space: 42.7 MB (92.71%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,23 +1,41 @@
+//SOLUTION 1
 class Solution {
-    static boolean myRecursion(int[] nArray, int k){
- 
-          if (k == 0)      
-            return true;      
-          else{
-              int i=0;
-                for(i=k-1;i>=0;i--){
-                    if(nArray[i]>=Math.abs(k-i))
-                        break;
-                }
-                if(i<0)
-                    return false;
-              return(myRecursion(nArray,i));
-          }      
-                  
-    }  
-    
     public boolean canJump(int[] nums) {
-        return myRecursion(nums,nums.length-1);
+        if(nums.length==0 || nums.length==1) return true;
+        int temp=nums[0];
+        if(temp==0) return false;
         
+        for(int i =1; i<nums.length-1;i++){
+            temp--;
+            if(temp<nums[i]) temp=nums[i];
+            if(temp==0) return false;
+        }
+        return true;
     }
 }
+
+//SOLUTION 2
+
+// class Solution {
+//     static boolean myRecursion(int[] nArray, int k){
+ 
+//           if (k == 0)      
+//             return true;      
+//           else{
+//               int i=0;
+//                 for(i=k-1;i>=0;i--){
+//                     if(nArray[i]>=Math.abs(k-i))
+//                         break;
+//                 }
+//                 if(i<0)
+//                     return false;
+//               return(myRecursion(nArray,i));
+//           }      
+                  
+//     }  
+    
+//     public boolean canJump(int[] nums) {
+//         return myRecursion(nums,nums.length-1);
+        
+//     }
+// }
