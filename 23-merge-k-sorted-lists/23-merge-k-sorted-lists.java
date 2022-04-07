f7acfe91ea355ccbcb3c7b99471d5d2135f24bff
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-04-07 09:24:29 +0530

    Time: 179 ms (13.85%), Space: 47 MB (66.71%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -8,23 +8,61 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//solution 1
 class Solution {
+    
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        
+     
+           if(list1.val<list2.val){
+               list1.next=mergeTwoLists(list1.next,list2);
+                return list1;               
+           }
+           else{
+               list2.next=mergeTwoLists(list1,list2.next);
+               return list2;
+           }        
+    }
+    
     public ListNode mergeKLists(ListNode[] lists) {
-   LinkedList<Integer> ll= new LinkedList<Integer>();
-        for(ListNode l : lists){
-            while(l!=null){
-                ll.add(l.val);
-                l=l.next;
-            }
-        }
-        Collections.sort(ll);
-        ListNode head=new ListNode();
-        ListNode h=head;
-        for(int i:ll){
-            ListNode t=new ListNode(i);
-            h.next=t;
-            h=t;
-        }
-        return head.next;
+        if(lists.length==0) return null;
+            ListNode temp=lists[0];
+        for(int i=1;i<lists.length;i++)
+            temp=mergeTwoLists(temp,lists[i]);      
+           return temp;
 }
+    
+    
+    
+    
 }
+
+
+
+
+
+// Solution 2
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//    LinkedList<Integer> ll= new LinkedList<Integer>();
+//         for(ListNode l : lists){
+//             while(l!=null){
+//                 ll.add(l.val);
+//                 l=l.next;
+//             }
+//         }
+//         Collections.sort(ll);
+//         ListNode head=new ListNode();
+//         ListNode h=head;
+//         for(int i:ll){
+//             ListNode t=new ListNode(i);
+//             h.next=t;
+//             h=t;
+//         }
+//         return head.next;
+// }
+// }
+
