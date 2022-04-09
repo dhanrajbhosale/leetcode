a85611f08d6550263cf53cec63f4a21dec85c66b
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-04-09 15:13:25 +0530

    Time: 7 ms (88.61%), Space: 57.3 MB (34.38%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -8,25 +8,70 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//Solution 1 Top Down Merge Sort that follows the Divide and Conquer Strategy.
+
 class Solution {
     public ListNode sortList(ListNode head) {
-        
-        LinkedList<Integer> ll=new LinkedList<Integer>();
-        
-        while(head!=null){
-            ll.add(head.val);
-            head=head.next;
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
         }
-        
-        Collections.sort(ll);
-        
-        ListNode head1=new ListNode();
-        ListNode h=head1;
-        for(int i:ll){
-            ListNode t=new ListNode (i);
-            h.next=t;
-            h=t;
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+
+    ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
         }
-        return head1.next;
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
     }
 }
+
+
+
+//Solution 2
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+        
+//         LinkedList<Integer> ll=new LinkedList<Integer>();
+        
+//         while(head!=null){
+//             ll.add(head.val);
+//             head=head.next;
+//         }
+        
+//         Collections.sort(ll);
+        
+//         ListNode head1=new ListNode();
+//         ListNode h=head1;
+//         for(int i:ll){
+//             ListNode t=new ListNode (i);
+//             h.next=t;
+//             h=t;
+//         }
+//         return head1.next;
+//     }
+// }
