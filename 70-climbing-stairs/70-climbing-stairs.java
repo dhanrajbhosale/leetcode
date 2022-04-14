f7c357202c15d3491acd973fac75d42eef926e93
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-04-14 22:01:54 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (84.25%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -57,23 +57,23 @@
  * Question   : Climbing Stairs
  * Complexity : Time: O(n) ; Space: O(n)
  */
-class Solution {
-    public int climbStairs(int n) {
-        if (n <= 1) {
-            return 1;
-        }
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n <= 1) {
+//             return 1;
+//         }
         
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+//         int[] dp = new int[n + 1];
+//         dp[1] = 1;
+//         dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
+//         for (int i = 3; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
 
-        return dp[n];
-    }
-}
+//         return dp[n];
+//     }
+// }
 
 
 /** DP + Optimization (Bottom Up Approach)
@@ -82,21 +82,21 @@ To calculate the new value we only leverage the previous two values. So we don't
  * Question   : Climbing Stairs
  * Complexity : Time: O(n) ; Space: O(1)
  */
-// class Solution {
-//     public int climbStairs(int n) {
-//         if (n <= 1) {
-//             return 1;
-//         }
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
 
-//         int prev1 = 1;
-//         int prev2 = 2;
+        int prev1 = 1;
+        int prev2 = 2;
 
-//         for (int i = 3; i <= n; i++) {
-//             int newValue = prev1 + prev2;
-//             prev1 = prev2;
-//             prev2 = newValue;
-//         }
+        for (int i = 3; i <= n; i++) {
+            int newValue = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = newValue;
+        }
 
-//         return prev2;
-//     }
-// }
+        return prev2;
+    }
+}
