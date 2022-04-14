0537dc2f7578c7d3f82e5537828118976990c3f4
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-04-15 00:08:49 +0530

    Time: 0 ms (100.00%), Space: 39 MB (90.06%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,4 +1,4 @@
-// Sol 1 recursion
+// Sol 1 recursion O(n^2) O(n)
 // class Solution {
 //     public int fib(int n) {
 //         if(n<=1) return n;
@@ -6,19 +6,34 @@
 //     }
 // }
 
-//Sol 2 recursion + memorization (DP)
-class Solution {
-    private int fib(int n, int []dp){
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fib(n-1,dp)+fib(n-2,dp);
-    }
+//Sol 2 recursion + memorization (DP) O()
+// class Solution {
+//     private int fib(int n, int []dp){
+//         if(dp[n]!=-1) return dp[n];
+//         return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+//     }
 
+//     public int fib(int n) {
+//         if(n<=0) return 0;
+//         int dp[]=new int [n+1];
+//         Arrays.fill(dp,-1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         return fib(n,dp);
+//     }
+// }
+
+//Sol 3 (Bottom Up)
+class Solution {
     public int fib(int n) {
-        if(n<=0) return 0;
-        int dp[]=new int [n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=0;
-        dp[1]=1;
-        return fib(n,dp);
+        if(n==0) return 0;
+        int prev1=1;
+        int prev2=0;
+            for(int i=2;i<=n;i++){
+                int val=prev1+prev2;
+                prev2=prev1;
+                prev1=val;
+            }
+        return prev1;
     }
 }
