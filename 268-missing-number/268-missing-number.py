82781a22fc52acc58230c8334da4dc8cbc34d4d9
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-05-29 01:01:37 +0530

    Time: 118 ms (74.89%), Space: 15 MB (14.02%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l=len(nums)
-        checks=[0]*(l+1)
-        for x in nums: 
-            checks[x]=1
-        return checks.index(0)
+        #Solution 1
+        n = len(nums)
+        return n * (n+1) / 2 - sum(nums) #sum of n terms - sum of list
+        
+        #Solution 2
+#         l=len(nums)
+#         checks=[0]*(l+1)
+#         for x in nums: 
+#             checks[x]=1
+#         return checks.index(0)
+        
         
