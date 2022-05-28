3db044fb40f8951b680bdac0459aee78cc2aaa54
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-05-29 00:42:49 +0530

    Time: 533 ms (91.37%), Space: 25.3 MB (62.45%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -4,11 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        temp=nums
-        temp.sort()
-        for a in range(1,len(temp)):
-            if temp[a-1]==temp[a]:
-                return temp[a]
+        #Solution 1
+        slow = fast = finder = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                while finder != slow:
+                    finder = nums[finder]
+                    slow = nums[slow]
+                return finder
+
+        
+        # Solution 2
+        # temp=nums
+        # temp.sort()
+        # for a in range(1,len(temp)):
+        #     if temp[a-1]==temp[a]:
+        #         return temp[a]
         
             
         
