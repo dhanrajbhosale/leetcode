8c0bf55d2b9746b2b4b51ad41f410287600291d5
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-05-29 16:12:43 +0530

    Time: 227 ms (45.80%), Space: 15.6 MB (35.10%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,3 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(nums)[len(nums)//2]
+        # solution 1 Using Sorting TC: O(nlogn) SC: O(1)
+        # return sorted(nums)[len(nums)//2]
+        
+        # solution 2 Using Disctionary TC: O(2n) SC: O(n)
+        # countEle = Counter(nums) 
+        # for key in countEle.keys():
+        #     if countEle[key] > len(nums) // 2:
+        #         return key
+        
+        # solution 3 Boyer Moore's Voting Algorith
+        majorEle, count = nums[0], 0
+    
+        for num in nums:
+            if num == majorEle:
+                count += 1
+            elif count == 0:
+                majorEle, count = num, 1
+            else:
+                count -= 1
+
+        return majorEle
