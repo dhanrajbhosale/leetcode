7aeae78b83ca2671676585465b3008b583b6511c
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-05-30 08:01:29 +0530

    Time: 239 ms (5.08%), Space: 32 MB (6.78%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 class Solution:
+    #Sol 1 recusrssive with dp
     def findPath(self, i, j, m, n, cnt)-> int:
         if i==m or j==n: return 1
         if cnt[i][j]!=0: return cnt[i][j]            
@@ -10,4 +11,12 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         cnt=np.zeros((m,n),dtype=int)
         return self.findPath(0,0,m-1,n-1, cnt)
+    
+    # Sol 2 Mathematical 
+    #     m-1 + n-1 total steps = m+n-2
+    #     M-1 steps must be down
+    #     N-1 steps must be to right 
+    # So total ways, select m-1 down steps position from total steps
+    
+        # return math.comb(m + n - 2, m - 1)
         
