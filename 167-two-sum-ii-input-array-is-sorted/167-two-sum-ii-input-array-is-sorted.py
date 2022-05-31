07456a9d14e921a619e260356a80d4211a2ffa07
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-05-31 14:39:56 +0530

    Time: 126 ms (96.67%), Space: 15 MB (42.04%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen={}
-        for i, value in enumerate(nums):
-            if value in seen:
-                return [seen[value]+1,i+1]
+        
+        # sol 2 two pntr approach, as list is sorted
+        l, r=0, len(nums)-1
+        while l<r:
+            if (nums[l]+nums[r])==target:
+                return [l+1,r+1]
+            elif (nums[l]+nums[r])>target:
+                r-=1
             else:
-                seen[target-value]=i
+                l+=1
+        
+        
+        # Sol 1 Using Dictionary TC: O(n) SC: O(n)
+        # seen={}
+        # for i, value in enumerate(nums):
+        #     if value in seen:
+        #         return [seen[value]+1,i+1] #since the values in seen has always lower indices than your current number, it should come first.
+        #     else:
+        #         seen[target-value]=i
