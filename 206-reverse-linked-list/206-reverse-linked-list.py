b1603e3518df1e010e670cdfdaf41ce882b6d57f
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-06-03 14:37:09 +0530

    Time: 41 ms (75.78%), Space: 15.4 MB (94.13%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,14 +4,26 @@
 #         self.val = val
 #         self.next = next
 
-# Recursion 
+# Sol 1 Recursive
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+#         newHead = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return newHead
+
+# Sol 2 Iterative
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        newHead = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return newHead
-        
+        prev = None
+        while head:
+            curr=head
+            head=head.next
+            curr.next=prev
+            prev=curr
+        return prev
+            
+            
         
