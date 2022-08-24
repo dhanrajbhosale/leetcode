2f5ee95ec9e47d9ed782bcec72a9ff1b9525e569
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-08-23 19:35:00 -0700

    Time: 60 ms (42.50%), Space: 15 MB (47.31%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -7,24 +7,68 @@ class Node:
         self.random = random
 """
 
+# optimum approach Time O(n), Space O(1)
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        oldToCopy = { None : None}
-        
+        if not head:
+            return
+        # step 1 make copy and add next to original
         curr = head
-        # makig a hashmap
         while curr:
+            front = curr.next
             copy = Node(curr.val)
-            oldToCopy[curr] = copy
-            curr = curr.next
-        
-        # update next and random pooint and make a copy list
-        curr = head  
+            curr.next = copy
+            copy.next = front
+            curr = front
+        # step 2 update random pntr of copy
+        curr = head
         while curr:
-            copy = oldToCopy[curr]
-            copy.next = oldToCopy[curr.next]
-            copy.random = oldToCopy[curr.random]
-            curr = curr.next
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr=curr.next.next
         
-        return oldToCopy[head]
+        # step 3 separate copy and original list
+        curr = head
+        copyHead = copy = head.next
+        while curr:
+            curr.next= copy.next
+            curr = curr.next
+            if curr:
+                copy.next = curr.next
+            else:
+                copy.next = None
+            copy = copy.next
+            
+        return copyHead
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+# with hashmap Time: O(n) Space: O(n)
+# class Solution:
+#     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+#         oldToCopy = { None : None}
+        
+#         curr = head
+#         # makig a hashmap
+#         while curr:
+#             copy = Node(curr.val)
+#             oldToCopy[curr] = copy
+#             curr = curr.next
+        
+#         # update next and random pooint and make a copy list
+#         curr = head  
+#         while curr:
+#             copy = oldToCopy[curr]
+#             copy.next = oldToCopy[curr.next]
+#             copy.random = oldToCopy[curr.random]
+#             curr = curr.next
+        
+#         return oldToCopy[head]
     
