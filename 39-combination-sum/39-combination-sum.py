572858599aed62bcf01e7dc29403873615196d41
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-08-27 10:56:24 -0700

    Time: 143 ms (44.58%), Space: 14.1 MB (36.45%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,20 +1,36 @@
+# Approach 1
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res=[]     
+#         def dfs(i, subset, total):
+#             if target==total:
+#                 res.append(subset[:])
+#                 return 
+#             if i>=len(candidates) or target<total:
+#                 return        
+#             #decision to take and START FROM SAME POINT with less sum
+#             subset.append(candidates[i])
+#             dfs(i, subset, total+candidates[i])
+#             #decision not to take
+#             subset.pop()
+#             dfs(i+1, subset, total)
+            
+#         dfs(0, [], 0)
+#         return res
+
+# approach 2
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res=[]
-        
-        def dfs(i, subset, total):
-            if target==total:
-                res.append(subset[:])
-                return 
-            if i>=len(candidates) or target<total:
+        res=[]     
+        def dfs(cands, subset, target):
+            if target<0:
                 return
+            if target == 0:
+                res.append(subset[:])
+                return
+            for i in range (len(cands)):
+                dfs(cands[i:], subset+[cands[i]], target-cands[i])
             
-            #decision to take and START FROM SAME POINT with less sum
-            subset.append(candidates[i])
-            dfs(i, subset, total+candidates[i])
-            #decision not to take
-            subset.pop()
-            dfs(i+1, subset, total)
             
-        dfs(0, [], 0)
-        return res
+        dfs(candidates, [], target)
+        return res    
