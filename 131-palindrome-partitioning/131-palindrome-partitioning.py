76b870d502dd19b04de07d8e5d7a0560e600df01
dhanrajbhosale <dhanrajbhosale7797@gmail.com>
2022-08-27 17:36:20 -0700

    Time: 857 ms (69.77%), Space: 28.9 MB (95.84%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,13 +1,17 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-
-        def dfs(start):
-            if start == len(s):
-                return [[]]
-            res = []
-            for i in range(start, len(s)):
-                cur = s[start:i+1]
-                if cur == cur[::-1]:
-                    res += [[cur] + rest for rest in dfs(i+1)]
-            return res
-        return dfs(0)
+        res=[]
+        self.dfs(s, [], res)
+        return res
+    
+    def isPal(self, s):
+        return s==s[::-1]
+        
+    def dfs(self, s, subset, res):
+        if not s:
+            res.append(subset[:])
+            return
+        # if just len(s), 'i' max will be len(s)-1, which will ignore the case of including last character during slicing.
+        for i in range(1,len(s)+1):         
+            if self.isPal(s[:i]):
+                self.dfs(s[i:], subset+[s[:i]], res)
