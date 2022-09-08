38cd63e1f281b1261f82785120e91ffcf3bd3567
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-09-07 19:28:14 -0700

    Time: 85 ms (83.04%), Space: 13.5 MB (45.06%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -9,15 +9,16 @@ class Solution(object):
                 if board[r][c]==".":
                     continue
                 k=board[r][c]
-                board[r][c]="."
+                board[r][c]="." #remove that value to avoid validating with itself
                 if self.isValidBox(board, r, c, k):
-                    board[r][c]=k
+                    board[r][c]=k # restore value removed for next validations
                     continue          
                 return False
         return True
                 
     def isValidBox(self, board, r, c, k):
         for i in range(9):
+            # check if same number present in rows, cols, box
             if board[r][i]==k or board[i][c]==k or board[3*(r//3)+(i//3)][3*(c//3)+(i%3)]==k:
                 return False
         return True
