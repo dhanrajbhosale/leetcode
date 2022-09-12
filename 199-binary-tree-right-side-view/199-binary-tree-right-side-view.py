ef1066d5807ee85fdb642e0f91092dd98bf4cf5b
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-09-11 22:59:09 -0700

    Time: 75 ms (5.43%), Space: 13.8 MB (97.98%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -8,15 +8,14 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         # root right left traversal
         res=[]
-        def recursion(root, level, res):
-            if not root:
-                return
-            if level==len(res):
-                res.append(root.val)
-            recursion(root.right, level+1, res)
-            recursion(root.left, level+1, res)
-        recursion(root, 0, res)
+        self.recursion(root, 0, res)
         return res
         
-    
+    def recursion(self, root, level, res):
+        if not root:
+            return
+        if level==len(res):
+            res.append(root.val)
+        self.recursion(root.right, level+1, res)
+        self.recursion(root.left, level+1, res)
         
