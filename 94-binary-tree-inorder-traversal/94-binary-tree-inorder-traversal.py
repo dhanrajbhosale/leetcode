0b73ebc21e97b09b533f15d03df14a3ea2bea654
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-09-11 18:36:46 -0700

    Time: 53 ms (43.43%), Space: 13.8 MB (59.72%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -6,5 +6,45 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right) if root else []
+        # recurrsive approach Time O(n) Space O(n) auxilary stack space
+        # return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right) if root else []
         
+        # iterative approach Time O(n) Space O(n)
+        # stack, res = [], []      
+        # while True:
+        #     while root:
+        #         stack.append(root)
+        #         root=root.left
+        #     if not stack: return res
+        #     node = stack.pop()
+        #     res.append(node.val)
+        #     root=node.right
+            
+        # Approach 3 rd - Morris Traversal Without Stack (using threaded BT) And Recurrsion Time O(n) Space O(1)
+        res = []
+        curr = root
+        
+        while curr:
+            # If left null, print curr and move right
+            if not curr.left:
+                res.append(curr.val)
+                curr = curr.right
+            # before going left, make right most node on left subtree connected to current node, then go left
+            else:
+                prev = curr.left
+                while prev.right and prev.right!=curr:
+                    prev = prev.right
+                # make thread
+                if not prev.right:
+                    prev.right = curr
+                    curr = curr.left
+                # if thread is already pointed to current node, means You have visited the node, cut the thread, print the root and  move to the right
+                else:
+                    prev.right = None
+                    res.append(curr.val)
+                    curr = curr.right
+        return res
+                    
+                    
+                    
+                
