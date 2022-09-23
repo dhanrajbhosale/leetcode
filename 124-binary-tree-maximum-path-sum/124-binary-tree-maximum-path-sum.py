8ee4064063a4c8f896bec6be8b26dfa3561ce86c
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-09-23 02:59:30 -0700

    Time: 191 ms (18.96%), Space: 21.4 MB (30.80%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -8,17 +8,18 @@ class Solution:
 #     Approch: calculate sum of left and right, update result if left right and curent node total is max, this max is for path from that current node, now return the max from left or right to parent node (only if greater than zero) to form other path combinations
  
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = [float('-inf')]
+        res = float('-inf')
         
         def dfs(root):
+            nonlocal res
             if not root:
                 return 0
             left = dfs(root.left)
             right = dfs(root.right)
             # storing max sum including current node
-            res[0] = max(res[0], root.val + left + right)
+            res = max(res, root.val + left + right)
             # returning left or rigt to form next path onlt if it's greater than ZERO, as -ve number won't add value
             return max(0, left + root.val, right + root.val)
         dfs(root)      
-        return res[0]
+        return res
     
