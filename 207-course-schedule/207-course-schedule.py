8f6f03c2c06f33d19ef1a65ca3c9519396659a50
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-09-29 11:15:10 -0700

    Time: 202 ms (38.31%), Space: 17.3 MB (18.76%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # detect cycle using DFS
+        # like normal dfs, we dont need to avoid parent as adj list are not given.. we are creating that list using prer list so already parent node is not present.
+        
+        vis = set()
+        adj  = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+        def dfs(node):
+            if adj[node]==[]:
+                return True
+            if node in vis:
+                return False
+            vis.add(node)
+            for i in adj[node]:
+                if not dfs(i): return False
+            adj[node]=[]
+            return True
+        # to consider disconnected graphs
+        for i in range(numCourses):
+            if(not dfs(i)): 
+                return False
+        return True
