3898c9918816fd4c8fdffefdf8149ec355784c61
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-01 14:59:07 -0700

    Time: 199 ms (44.44%), Space: 15.6 MB (71.38%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # this is topological sort problem
+        # using BFS (Kahn's Algorithm)
+        
+        inDegree = [0 for i in range(numCourses)]
+        adj = {i:[] for i in range(numCourses)}
+        res=[]
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+            inDegree[pre]+=1
+        q = collections.deque()
+        
+        for i in range(numCourses):
+            if inDegree[i]==0:
+                q.append(i)
+        while q:
+            v = q.popleft()
+            res.append(v)
+            for i in adj[v]:
+                inDegree[i]-=1
+                if inDegree[i]==0:
+                    q.append(i)
+        return res[::-1] if len(res)==numCourses else []
+                
