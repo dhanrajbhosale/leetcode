8b15834fb643229a4ef3e941f6f489cb52fff620
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-01 18:47:20 -0700

    Time: 246 ms (20.70%), Space: 17.6 MB (21.54%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,30 +1,63 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # topological sort DFS
+        vis, cycle = set(), set()
+        res = []
+        prer={i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            prer[crs].append(pre)
+        
+        # visited -> crs has been added to res
+        # visiting -> crs not added to res but added to the cycle
+        # unvisited -> crs not added to the res or cycle
+        
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in vis:
+                return True
+            
+            cycle.add(crs)
+            for i in prer[crs]:
+                if not dfs(i):
+                    return False
+            cycle.remove(crs)
+            vis.add(crs)
+            res.append(crs)
+            return True
+            
+            
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return res
+
+                
         # this is topological sort problem
         # using BFS (Kahn's Algorithm)
         
-        inDegree = [0 for i in range(numCourses)]
-        adj = {i:[] for i in range(numCourses)}
-        res=[]
-        for crs, pre in prerequisites:
-            adj[crs].append(pre)
-            inDegree[pre]+=1
-        q = collections.deque()
+#         inDegree = [0 for i in range(numCourses)]
+#         adj = {i:[] for i in range(numCourses)}
+#         res=[]
+#         for crs, pre in prerequisites:
+#             adj[crs].append(pre)
+#             inDegree[pre]+=1
+#         q = collections.deque()
         
-        for i in range(numCourses):
-            if inDegree[i]==0:
-                q.append(i)
-        while q:
-            v = q.popleft()
-            res.append(v)
-            for i in adj[v]:
-                inDegree[i]-=1
-                if inDegree[i]==0:
-                    q.append(i)
-        return res[::-1] if len(res)==numCourses else []
+#         for i in range(numCourses):
+#             if inDegree[i]==0:
+#                 q.append(i)
+#         while q:
+#             v = q.popleft()
+#             res.append(v)
+#             for i in adj[v]:
+#                 inDegree[i]-=1
+#                 if inDegree[i]==0:
+#                     q.append(i)
+#         return res[::-1] if len(res)==numCourses else []
     
 
-    # 99.6% faster
+    # 99.6% faster BFS 
 # class Solution:
 #     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 #         # Create a prerequisite dict. (containing courses (nodes) that need to be taken (visited)
