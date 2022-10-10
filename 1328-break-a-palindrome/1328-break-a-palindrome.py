130301a99988547a500dd984b6b95095d53608cf
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-10 13:12:06 -0700

    Time: 56 ms (40.63%), Space: 14 MB (12.92%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -3,13 +3,7 @@ class Solution:
         l= len(palindrome)
         if l<=1:
             return ""
-        for i in range(l):
-            if palindrome[i]!="a" and (i!=l//2 or l%2==0):
-                palindrome=palindrome[:i]+"a"+palindrome[i+1:]
-                break
-            if i==l-1:
-                palindrome=palindrome[:i]+"b"+palindrome[i+1:]
-        return palindrome
-                
-                
-        
+        for i in range(l//2):
+            if palindrome[i]!="a":
+                return palindrome[:i]+"a"+palindrome[i+1:]
+        return palindrome[:-1]+"b"
