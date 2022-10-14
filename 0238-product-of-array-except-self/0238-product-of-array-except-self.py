4a2c86a9beff970fe3cb1ba52614b8691765abc6
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-13 18:09:15 -0700

    Time: 207 ms (99.97%), Space: 21.4 MB (58.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,38 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        masterMul = 1   
-        zeroCnt = 0
-        for i in nums:
-            if i:
-                masterMul*=i
-            else:
-                zeroCnt+=1
-            if zeroCnt>=2:
-                return [0]*len(nums)
-            
-        if zeroCnt==1:
-            i = nums.index(0)
-            nums = [0]*len(nums)
-            nums[i] = masterMul
-            return nums  
-                
+        # left to right, right to left
+        res = [1] * len(nums)
+        
+        preFix = 1
         for i in range(len(nums)):
-            nums[i]=masterMul//nums[i]
-        return nums
+            res[i] = preFix
+            preFix *=nums[i]
         
-        
-        
+        postFix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postFix
+            postFix *=nums[i]
+
+        return res
+            
+            
+#       broot force
+#         masterMul = 1   
+#         zeroCnt = 0
+#         for i in nums:
+#             if i:
+#                 masterMul*=i
+#             else:
+#                 zeroCnt+=1
+#             if zeroCnt>=2:
+#                 return [0]*len(nums)
+            
+#         if zeroCnt==1:
+#             i = nums.index(0)
+#             nums = [0]*len(nums)
+#             nums[i] = masterMul
+#             return nums  
+                
+#         for i in range(len(nums)):
+#             nums[i]=masterMul//nums[i]
+#         return nums
