cb497a6b63afd6893be6016a03eac5d0093dfafa
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-16 19:29:34 -0700

    Time: 1485 ms (68.22%), Space: 14.8 MB (58.23%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,17 +1,22 @@
-import functools
 class Solution:
 
     def minDifficulty(self, A, d):
         n = len(A)
         if n < d: return -1
-
-        @functools.lru_cache(None)
+        
+        dp=[[-1 for _ in range(n)] for _ in range(d+1)]
+        
+        # @cache
+        # @functools.lru_cache()
         def dfs(i, d):
             if d == 1:
                 return max(A[i:])
+            if dp[d][i]!=-1:
+                return dp[d][i]
             res, maxd = float('inf'), 0
             for j in range(i, n - d + 1):
                 maxd = max(maxd, A[j])
                 res = min(res, maxd + dfs(j + 1, d - 1))
+            dp[d][i]=res
             return res
         return dfs(0, d)
