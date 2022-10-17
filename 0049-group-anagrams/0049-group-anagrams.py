abafe9fa448aa230bfd35f8f9c329a2d0243f7b5
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-17 01:46:06 -0700

    Time: 238 ms (32.03%), Space: 17.9 MB (55.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dic = collections.defaultdict(list)
-        res = []
-        for i, v in enumerate(strs):
-            dic["".join(sorted(v))].append(i)
-        for i in dic:
-            res.append([strs[i] for i in dic[i]])
-        return res
+        for v in strs:
+            dic["".join(sorted(v))].append(v)
+        return dic.values()
+    
