65a04f1b5f7c2970a7d41d23a13accdfd3db9eab
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-22 13:57:08 -0700

    Time: 133 ms (82.86%), Space: 14.7 MB (83.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,35 +3,27 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "": return t
         countT, window = defaultdict(int), defaultdict(int)
-        for c in t: # store required dict
-            countT[c]+=1
-            
-        l = 0 # left of current window   
-        have, need = 0, len(countT) # unique char, have and need
+        for c in t:
+            countT[c]+=1    
+        l = 0   
+        have, need = 0, len(countT) 
         min_window = float('inf') 
         res = [-1, -1]
         
-        for r in range(len(s)): # for every char in s
+        for r in range(len(s)):
             char = s[r]
-            window[char]+=1 # add char in window
+            window[char]+=1
             
-            # if char is our need and if we get exact count required, increment have
             if char in countT and window[char]==countT[char]:
                 have +=1
-            
-            # while we have all needed -  shrink window from left to get best, store min length
             while have == need:
-                
-                # store minimum window
                 if (r-l+1) < min_window:
                     min_window = (r-l+1)
                     res = [l, r]
                 
                 window[s[l]]-=1
-                # decrement have if removed char was needed and its cnt less that required
                 if s[l] in countT and window[s[l]]<countT[s[l]]:
                     have-=1
-                # shrink more
                 l+=1
         l, r = res
         return s[l:r+1] if min_window!= float('inf') else ""
