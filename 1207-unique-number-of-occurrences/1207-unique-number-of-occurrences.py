7ec61c5e27eb2e12faaf20410157c6766e745f7a
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-22 15:35:30 -0700

    Time: 72 ms (39.16%), Space: 14 MB (71.71%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,7 +1,6 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         dic = collections.defaultdict(int)
-        dic_set = set()
         for num in arr:
             dic[num]+=1
         return len(dic.values()) == len(set(dic.values()))
