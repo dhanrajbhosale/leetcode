30af6db8c85b1d72ccbaae2ae55b8db2e1e84e55
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-25 11:36:13 -0700

    Time: 83 ms (5.01%), Space: 13.9 MB (75.82%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,4 +1,10 @@
-class Solution:       
+class Solution:    
+    # TC O(n) SC O(n)
+    # def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        # return "".join(word1)=="".join(word2)
+    
+    # TC O(min(m,n)) => exits when it finds the first mismatch character
+    # SC O(1) => comparison on the fly.
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         for c1, c2 in zip(self.generate(word1), self.generate(word2)):
             if c1 != c2:
