14abfc43fe82f773d8209fca6b89608f62539909
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-28 10:34:34 -0700

    Time: 53 ms (63.84%), Space: 14 MB (7.83%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,5 +1,17 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        #Sol1: with build in function
+        # return bin(n).count('1')
+        
+        # Sol2: shift all 32 bits (need iterate all 32 times) to right and take out right most bit
+        # ones = 0
+        # while n:
+        #     ones+=(n&1)
+        #     n = n>>1
+        # return ones
+        
+        # Sol3: only iterate as number of 1s
+        # each time you use K & (K-1), the rightmost bit with value 1 (and only that bit) will be set to 0
         c = 0
         while n:
             n &= n - 1
