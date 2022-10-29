52d4243a2ffaad72ca8b788eeda28507d932bd63
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-10-29 16:12:10 -0700

    Time: 55 ms (48.01%), Space: 13.9 MB (12.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     # sol1 dp bottom up TC O(n) SC O(n)
-    @functools.lru_cache()
     def climbStairs(self, n: int) -> int:
-        dp=[None]*(n+2)
+        if n==1: return 1
+        dp=[None]*(n+1)
         dp[1], dp[2]= 1, 2
         for i in range(3, n+1):
             dp[i] = dp[i-1]+dp[i-2]
