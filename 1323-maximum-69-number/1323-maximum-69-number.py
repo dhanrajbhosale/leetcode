6a1b4feac876416b1ff6e7d84aee63c3ba0e27b9
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-11-07 16:31:16 -0800

    Time: 53 ms (61.48%), Space: 13.7 MB (95.47%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,4 +1,22 @@
 class Solution:
+    
+    # sol 2
     def maximum69Number (self, num: int) -> int:
-        return int(str(num).replace('6','9',1))
+        curr_digit = 0
+        index_first_six = -1
+        num_copy = num
+        
+        while num_copy:
+            if num_copy % 10 == 6:
+                index_first_six = curr_digit
+            num_copy //= 10
+            curr_digit += 1
+            
+        return num if index_first_six == -1 else num + 3 * 10 ** index_first_six
+    
+    # sol 1
+    # def maximum69Number (self, num: int) -> int:
+    #     while num:
+    #         c = num
+    #     return int(str(num).replace('6','9',1))
         
