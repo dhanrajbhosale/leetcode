99bc815c43a7eade8eca296696738996ac265302
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-11-10 14:59:07 -0800

    Time: 442 ms (88.98%), Space: 19.6 MB (40.94%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -1,11 +1,19 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stack = []
+        self.stack = [] # pair or (price, span)
 
-    def next(self, price):
-        res = 1
-        while self.stack and self.stack[-1][0] <= price:
-            res += self.stack.pop()[1]
-        self.stack.append([price, res])
-        return res
+    def next(self, price: int) -> int:
+        span = 1
+        # while pop is less than price pop it and increment span
+        # as we dont need to consider these elements as we are storing their span
+        while self.stack and self.stack[-1][0]<=price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        # even if price will be less, we need to return 1
+        self.stack.append([price, span])
+        
+        return span
+
+# Your StockSpanner object will be instantiate  d and called as such:
+
