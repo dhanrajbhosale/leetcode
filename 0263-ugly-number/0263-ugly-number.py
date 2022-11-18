18916b6096b8be536cbd122519927a0de6f57b99
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-11-18 16:34:34 -0700

    Time: 49 ms (75.13%), Space: 13.8 MB (97.12%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def isUgly(self, num: int) -> bool:
-        for p in 2, 3, 5:
-            while num % p == 0 < num:
-                num /= p
-        return num == 1
+        if num ==1: return True
+        for d in [2,3,5]:
+            while num:
+                q, r = divmod(num, d)
+                if q==1 and not r:
+                    return True
+                if r:
+                    break
+                num = q
+        return False
+                    
+                
