e59bb3845b77b1dea71e68f7c93c0394b740706a
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2022-11-18 22:38:54 -0700

    Time: 1757 ms (15.11%), Space: 14 MB (75.58%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,23 +3,21 @@ class Solution:
         self.solve(board)
     
     def solve(self, board: List[List[str]])->bool:
-        for r in range(len(board[0])): # solve row by row
+        for r in range(len(board[0])):
             for c in range(len(board)):
-                if board[r][c]==".": # if place vacant
-                    for i in range(1,10): # try putting 1-9 numbers 1 by 1
-                        if self.isValid(str(i), r, c, board): # CONSTRAIN
-                            board[r][c]= str(i) # Make CHOICE
-                            if self.solve(board): # solve this new Sudoku
+                if board[r][c]==".":
+                    for i in range(1,10): 
+                        if self.isValid(str(i), r, c, board):
+                            board[r][c]= str(i)
+                            if self.solve(board):
                                 return True
                             else:
-                                board[r][c]="." # undo CHOICE
+                                board[r][c]="."
                     return False            
-        return True # GOAL - all vacant places filled
-    
+        return True
             
     def isValid(self, n:str, r:int, c:int, board: List[List[str]])->bool:
         for i in range(9):
-            # check row, column, particular sub box
             if board[r][i]==n or board[i][c]==n or board[3*(r//3)+(i//3)][3*(c//3)+(i%3)]==n:
                 return False
         return True
