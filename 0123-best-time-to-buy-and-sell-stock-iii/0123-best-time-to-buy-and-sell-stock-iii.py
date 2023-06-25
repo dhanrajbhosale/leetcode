1a56ca3104360bfc3942325081705b82568d4497
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2023-06-25 08:31:19 -0700

    Time: 4840 ms (5.02%), Space: 207.4 MB (8.79%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -7,15 +7,15 @@ class Solution:
         if day == len(prices) or cap==0:
             return 0
         
-        if mem[day][buy][cap]!= -1: # DP LINE 1
-            return mem[day][buy][cap]
+        if (buy, day, cap) in mem: # DP LINE 1
+            return mem[(buy, day, cap)]
         
         if buy:  # (buy, not buy)
-            mem[day][buy][cap] = max(-prices[day] + self.get_profit_recc(0, day+1, cap, prices, mem), self.get_profit_recc(1, day+1, cap, prices, mem))
+            mem[(buy, day, cap)] = max(-prices[day] + self.get_profit_recc(0, day+1, cap, prices, mem), self.get_profit_recc(1, day+1, cap, prices, mem))
         else:  # (sell, not sell)
-            mem[day][buy][cap] = max(prices[day] + self.get_profit_recc(1, day+1, cap-1, prices, mem), self.get_profit_recc(0, day+1, cap, prices, mem))
-        return mem[day][buy][cap]
+            mem[(buy, day, cap)] = max(prices[day] + self.get_profit_recc(1, day+1, cap-1, prices, mem), self.get_profit_recc(0, day+1, cap, prices, mem))
+            
+        return mem[(buy, day, cap)]
 
     def maxProfit(self, prices: List[int]) -> int:
-        mem = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(len(prices))]
-        return self.get_profit_recc(1, 0, 2, prices, mem)
+        return self.get_profit_recc(1, 0, 2, prices, {})
