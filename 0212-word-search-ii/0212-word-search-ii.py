87819e1e867b5c178998cca0c125062c2e09a30f
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2023-07-08 12:16:34 -0700

    Time: 8857 ms (11.65%), Space: 19.4 MB (45.71%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,47 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.EndOfWord = False
+        
+    def add_word(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c]=TrieNode()
+            cur = cur.children[c]
+        cur.EndOfWord = True
+
+class Solution:
+    num_of_words = 0
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        self.num_of_words = len(words)
+        root = TrieNode()
+        for word in words:
+            root.add_word(word)  
+        res, visit = [], set()
+
+        def dfs(x, y, word, node):
+            if x<0 or y<0 or x>=len(board) or y>=len(board[0]) or board[x][y] not in node.children or (x,y) in visit or self.num_of_words == 0:
+                return
+            visit.add((x,y))
+            
+            node = node.children[board[x][y]]
+            word+=board[x][y]
+            if node.EndOfWord:
+                res.append(word)
+                node.EndOfWord = False
+                self.num_of_words-=1
+            
+            dfs(x+1, y, word, node)
+            dfs(x, y+1, word, node)
+            dfs(x-1, y, word, node)
+            dfs(x, y-1, word, node)
+
+            visit.remove((x,y))
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                dfs(i, j, "", root)
+
+        return res
+        
