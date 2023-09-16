c106eff9d1b9ac11673e0c1c7dfc1c5905b5bc12
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2023-09-16 00:51:31 -0700

    Time: 422 ms (55.88%), Space: 18.2 MB (92.99%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,31 @@
+
+# Standard Djikstraa
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
+        adj = collections.defaultdict(list)
+        for u, v, w in times:
+            adj[u].append([w, v])
+            # adj[v].append([w, u])
+        
+        # edge, destination
+        minHeap = [[0, k]] 
+        visited = set()
+        ans = 0
+        # while len(visited)<n:
+        while minHeap:
+            path, source = heapq.heappop(minHeap)
+            if source in visited:
+                continue
+            visited.add(source)
+            ans = max(ans, path)
+            for edge, destination in adj[source]:
+                if destination in visited:
+                    continue 
+                heapq.heappush(minHeap, [path+edge, destination])
+            
+        # if not visited all nodes, return -1
+        return ans if len(visited)==n else -1
+            
+            
+            
