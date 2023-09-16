451af797b25fa3371f6a2a9c04f82fdc2c306deb
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2023-09-15 19:38:37 -0700

    Time: 2648 ms (30.57%), Space: 177.1 MB (24.22%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        # graph = defaultdict(list)
+        graph = {i : [] for i in range(n)}
+        def get_manhattan_dist(p1, p2):
+            return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+        
+        # Connect each pair of points with a weighted edge
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = get_manhattan_dist(points[i], points[j])
+                graph[i].append([dist, j])
+                graph[j].append([dist, i])
+        
+        # implement minimum spanning tree
+        # prim's algorrithm
+        # point, cost
+        res = 0
+        visited = set()
+        minHeap = [[0, 0]]
+        
+        while len(visited)<n:
+            cost, i = heapq.heappop(minHeap)
+            if i in visited: 
+                continue
+            res+= cost
+            visited.add(i)
+            
+            
+            for neiCost, nei in graph[i]:
+                if nei not in visited:
+                    heapq.heappush(minHeap, [neiCost, nei])
+            
+        return res
+                
+        
+        
