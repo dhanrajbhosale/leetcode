4de806c65d248a97c3e72edb582a1c1570e7271f
Dhanraj Bhosale <dhanrajbhosale7797@gmail.com>
2023-10-26 13:56:23 -0700

    Time: 403 ms (49.16%), Space: 16.5 MB (68.07%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -1,18 +1,15 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         arr.sort()
-        print(arr)
         bt_dict = {i:1 for i in arr}
-
+        total_bts = 0
         for  i in range(1, len(arr)):
-            parent = arr[i]
-            
+            parent = arr[i]     
             for j in range(i-1,-1,-1):
                 child1 = arr[j]
                 child2 = parent/child1
                 # found pair
                 if child2 in bt_dict:
-                    print(parent, child1, child1)
                     bt_dict[parent]+= bt_dict[child1]*bt_dict[child2]
-
-        return sum(bt_dict.values())%1000000007
+            total_bts+=bt_dict[parent]%1000000007
+        return (total_bts+1)%1000000007
